4367752af518da19f90ee111f2b9530999930279
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-02-05 23:00:33 -0300

    chore: http por https

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   function onSearch(ciudad) {
     try {
       fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`
       )
         .then((r) => r.json())
         .then((recurso) => {
