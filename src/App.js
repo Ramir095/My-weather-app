abd68e0fdab9e8ba03e523240d99b74df1c84abf
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-02-10 17:56:37 -0300

    chore

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import SearchBar from "./components/SearchBar";
 import Card from "./components/Card";
 import styles from "./App.module.css";
 import { useState } from "react";
-const apiKey = process.env.REACT_APP_APIKEY;
-// import { apiFuntion } from './controllers/api'
+import { apiFuntion } from './controllers/api'
 
 function App() {
   const [cities, setCities] = useState([]);
@@ -21,39 +20,11 @@ function App() {
 
     if (cityFound) return alert("la ciudad ya existe");
     
-    if(cities.length >= 4){
+     apiFuntion(ciudad, setCities)
+     
+     if(cities.length >= 4){
       cities.shift()
     }
-    try {
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`
-      )
-        .then((r) => r.json())
-        .then((recurso) => {
-          if (recurso.main !== undefined) {
-            const ciudad = {
-              min: Math.round(recurso.main.temp_min),
-              max: Math.round(recurso.main.temp_max),
-              humidity: recurso.main.humidity,
-              img: recurso.weather[0].icon,
-              id: recurso.id,
-              wind: Math.round(recurso.wind.speed),
-              temp: recurso.main.temp,
-              name: recurso.name,
-              weather: recurso.weather[0].main,
-              clouds: recurso.clouds.all,
-              latitud: recurso.coord.lat,
-              longitud: recurso.coord.lon,
-            };
-            setCities((oldCities) => [...oldCities, ciudad]);
-          } else {
-            alert("No encontramos datos de esa ciudad");
-          }
-        });
-    } catch (error) {
-      console.log("error", error);
-    }
-    // return apiFuntion(ciudad, setCities)
   }
 
   const lastCity = cities[cities.length - 1];
